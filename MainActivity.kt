4cfd833506e7f1d52d8c91c6b8d37bcd8e60e333
DeekshithBV <127478832+DeekshithBV@users.noreply.github.com>
2024-04-13 22:13:20 +0530

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -2,28 +2,57 @@ package com.example.makingtoast
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton: Button;
-    private lateinit var falseButton: Button;
+    private lateinit var buttonTrue: Button
+    private lateinit var buttonFalse: Button
+    private lateinit var buttonNext: Button
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia,true),
+        Question(R.string.question_oceans,true),
+        Question(R.string.question_mideast,false),
+        Question(R.string.question_africa,false)
+    )
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //getting references to widgets
-        trueButton = findViewById(R.id.true_button);
-        falseButton = findViewById(R.id.false_button);
+        buttonTrue = findViewById(R.id.button_true)
+        buttonFalse = findViewById(R.id.button_false)
+        buttonNext = findViewById(R.id.button)
+        questionTextView = findViewById(R.id.text)
 
-        //setting listeners
-        trueButton.setOnClickListener {
-            Toast.makeText(this,"Wrong answer",Toast.LENGTH_LONG).show()
+        buttonTrue.setOnClickListener {
+            checkAnswer(true)
         }
-        falseButton.setOnClickListener {
-            Toast.makeText(this,"Correct answer",Toast.LENGTH_LONG).show()
+        buttonFalse.setOnClickListener {
+            checkAnswer(false)
         }
+        buttonNext.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
+    }
+    private fun updateQuestion(){
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+    private fun checkAnswer(userAnswer: Boolean){
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer){
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show()
     }
 }
